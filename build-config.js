e7f28aa5c561be82f3d91645b391bc8a4ef00b75
adminabious <adminabious@example.com>
2026-06-29 13:48:14 +0300

    Fix Render build script path

--- build-config.js
+++ build-config.js
@@ -1,13 +1,1 @@
-const fs = require('fs');
-const path = require('path');
-
-const config = {
-  WHATSAPP_NUMBER: process.env.WHATSAPP_NUMBER || '44743960764',
-  BIRTHDAY_DATE: process.env.BIRTHDAY_DATE || '07/07'
-};
-
-const outputPath = path.join(__dirname, 'birthday', 'env-config.js');
-const fileContents = `window.APP_CONFIG = ${JSON.stringify(config, null, 2)};\n`;
-
-fs.writeFileSync(outputPath, fileContents, 'utf8');
-console.log(`Generated ${outputPath}`);
+require('./birthday/build-config.js');
