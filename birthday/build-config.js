1422a0f585fef92b3e01c3867a9cd87ab35693ad
adminabious <adminabious@example.com>
2026-06-30 14:13:35 +0300

    Update WhatsApp gift number

--- birthday/build-config.js
+++ birthday/build-config.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const config = {
-  WHATSAPP_NUMBER: process.env.WHATSAPP_NUMBER || '44743960764',
+  WHATSAPP_NUMBER: process.env.WHATSAPP_NUMBER || '256743960764',
   BIRTHDAY_DATE: process.env.BIRTHDAY_DATE || '07/07'
 };
 
